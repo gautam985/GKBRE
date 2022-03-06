15b4629f0f7b2afa21ee07b6d5588f5f800a6966
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 01:59:07 +0530

    Implemenation for Physical Product

--- GK.BRE/GK.BRE/BC/BusinessRules/PhysicalProductBR.cs
+++ GK.BRE/GK.BRE/BC/BusinessRules/PhysicalProductBR.cs
@@ -1,16 +1,39 @@
 ﻿using GK.BRE.Models;
+using GK.BRE.Models.Enums;
 using GK.BRE.Models.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace GK.BRE.BC.BusinessRules
 {
     public class PhysicalProductBR : IBusinessRule
     {
-        public string Name => throw new NotImplementedException();
+        public string Name => "Physical Product Business Rule";
 
         public Result ExecuteRule(Product product)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (product.ProductType == ProductType.Physical)
+                {
+                    // Create packing slip for the shipping
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Success,
+                        Comments = "Packing slip for the shipping created successfully"
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                return new Result
+                {
+                    ResultCode = ResultCode.Error,
+                    Errors = new List<string> { e.Message }
+                };
+            }
         }
     }
 }
