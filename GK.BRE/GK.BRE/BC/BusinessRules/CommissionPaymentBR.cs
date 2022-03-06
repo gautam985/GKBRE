162e8f5aca394a26f52fd2fc7c53a9dc276a0305
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 02:03:53 +0530

    Implementation for Commission Payment

--- GK.BRE/GK.BRE/BC/BusinessRules/CommissionPaymentBR.cs
+++ GK.BRE/GK.BRE/BC/BusinessRules/CommissionPaymentBR.cs
@@ -1,16 +1,39 @@
 ﻿using GK.BRE.Models;
+using GK.BRE.Models.Enums;
 using GK.BRE.Models.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace GK.BRE.BC.BusinessRules
 {
     public class CommissionPaymentBR : IBusinessRule
     {
-        public string Name => throw new NotImplementedException();
+        public string Name => "Commission Payment Business Rule";
 
         public Result ExecuteRule(Product product)
         {
-            throw new NotImplementedException();
+            if (product.ProductType == ProductType.Book || product.ProductType == ProductType.Physical)
+            {
+                try
+                {
+                    //Create commission payment for the agent
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Success,
+                        Comments = "Generated commission payment to the agent successfully"
+                    };
+                }
+                catch (Exception e)
+                {
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Error,
+                        Errors = new List<string> { e.Message }
+                    };
+                }
+            }
+
+            return null;
         }
     }
 }
