1996fb53fa8764ac9b6abb9cf3d20e65c89a693e
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 01:33:29 +0530

    Added unit test for Physical Product

--- GK.BRE.UnitTest/GK.BRE.UnitTest/BusinessRuleExecutionTest.cs
+++ GK.BRE.UnitTest/GK.BRE.UnitTest/BusinessRuleExecutionTest.cs
@@ -1,13 +1,36 @@
+using GK.BRE.BC;
+using GK.BRE.Models;
+using GK.BRE.Models.Enums;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace GK.BRE.UnitTest
 {
     [TestClass]
     public class BusinessRuleExecutionTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private BusinessRulesEngine _businessRulesEngine;
+
+        #region Private Method
+
+        private void InitilizeBusinessObjects()
         {
+            _businessRulesEngine = new BusinessRulesEngine();
         }
+
+        #endregion Private Method
+
+        #region Test Method
+
+        [TestMethod]
+        public void PhysicalProductTest()
+        {
+            InitilizeBusinessObjects();
+            var result = _businessRulesEngine.ExecuteBusinessRules(new List<Product>() { new Product { ProductType = ProductType.Physical } });
+
+            Assert.IsTrue(result[0].Results.Count == 2);
+        }
+
+        #endregion Test Method
     }
 }
