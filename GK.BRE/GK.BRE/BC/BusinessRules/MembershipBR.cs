f92a2b8f152f4d10b160c854975af238424e659b
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 02:07:10 +0530

    Implemenation for Membership

--- GK.BRE/GK.BRE/BC/BusinessRules/MembershipBR.cs
+++ GK.BRE/GK.BRE/BC/BusinessRules/MembershipBR.cs
@@ -1,16 +1,48 @@
 ﻿using GK.BRE.Models;
+using GK.BRE.Models.Enums;
 using GK.BRE.Models.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace GK.BRE.BC.BusinessRules
 {
     public class MembershipBR : IBusinessRule
     {
-        public string Name => throw new NotImplementedException();
+        public string Name => "Membership Business Rule";
 
         public Result ExecuteRule(Product product)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (product.ProductType == ProductType.Membership)
+                {
+                    // Activate membership
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Success,
+                        Comments = "Membership activated successfully"
+                    };
+                }
+                else if (product.ProductType == ProductType.MembershipUpgrade)
+                {
+                    // Upgrade membership
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Success,
+                        Comments = "Membership upgraded successfully"
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                return new Result
+                {
+                    ResultCode = ResultCode.Error,
+                    Errors = new List<string> { e.Message }
+                };
+            }
         }
     }
 }
