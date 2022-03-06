03d40f423acea03b8cde555818243f2aa7898675
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 02:32:57 +0530

    Added Product Orders Controller

---
+++ GK.BRE/GK.BRE/Controllers/ProductOrdersController.cs
@@ -0,0 +1,28 @@
+﻿using GK.BRE.Models;
+using GK.BRE.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace GK.BRE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductOrdersController : ControllerBase
+    {
+        private readonly ILogger<ProductOrdersController> _logger;
+        private readonly IBusinessRulesEngine _businessRulesEngine;
+
+        public ProductOrdersController(ILogger<ProductOrdersController> logger, IBusinessRulesEngine businessRulesEngine)
+        {
+            _logger = logger;
+            _businessRulesEngine = businessRulesEngine;
+        }
+
+        [HttpPost]
+        public List<ProductBusinessRuleExecutionResult> Post([FromBody] List<Product> products)
+        {
+            return _businessRulesEngine.ExecuteBusinessRules(products);
+        }
+    }
+}
--- GK.BRE/GK.BRE/Startup.cs
+++ GK.BRE/GK.BRE/Startup.cs
@@ -1,14 +1,10 @@
+using GK.BRE.BC;
+using GK.BRE.Models.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace GK.BRE
 {
@@ -26,6 +22,7 @@ namespace GK.BRE
         {
 
             services.AddControllers();
+            services.AddSingleton<IBusinessRulesEngine, BusinessRulesEngine>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
