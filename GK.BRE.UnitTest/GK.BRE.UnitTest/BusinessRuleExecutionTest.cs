3fc0a6728f3bd62cf4819cdd835e1110803f8dc1
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 01:28:01 +0530

    Added Unit Test project

---
+++ GK.BRE.UnitTest/GK.BRE.UnitTest/BusinessRuleExecutionTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GK.BRE.UnitTest
+{
+    [TestClass]
+    public class BusinessRuleExecutionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
