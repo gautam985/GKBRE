becb263cdb22a52a70580f37558831f36c443939
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 02:09:39 +0530

    Implementation for Email Notification

--- GK.BRE/GK.BRE/BC/BusinessRules/MembershipEmailNotificationBR.cs
+++ GK.BRE/GK.BRE/BC/BusinessRules/MembershipEmailNotificationBR.cs
@@ -1,16 +1,39 @@
 ﻿using GK.BRE.Models;
+using GK.BRE.Models.Enums;
 using GK.BRE.Models.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace GK.BRE.BC.BusinessRules
 {
     public class MembershipEmailNotificationBR : IBusinessRule
     {
-        public string Name => throw new NotImplementedException();
+        public string Name => "Membership Email Notification Business Rule";
 
         public Result ExecuteRule(Product product)
         {
-            throw new NotImplementedException();
+            if (product.ProductType == ProductType.Membership || product.ProductType == ProductType.MembershipUpgrade)
+            {
+                try
+                {
+                    //Send email notification to the subscriber for the membership activation/upgrade.
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Success,
+                        Comments = "Email notification sent successfully"
+                    };
+                }
+                catch (Exception e)
+                {
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Error,
+                        Errors = new List<string> { e.Message }
+                    };
+                }
+            }
+
+            return null;
         }
     }
 }
