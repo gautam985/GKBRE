60f4c3499aac70bf963e1a637e714efce4c0182f
gautam985 <71507912+gautam985@users.noreply.github.com>
2022-03-07 02:01:36 +0530

    Implementation for book

--- GK.BRE/GK.BRE/BC/BusinessRules/BookBR.cs
+++ GK.BRE/GK.BRE/BC/BusinessRules/BookBR.cs
@@ -1,16 +1,39 @@
 ﻿using GK.BRE.Models;
+using GK.BRE.Models.Enums;
 using GK.BRE.Models.Interfaces;
 using System;
+using System.Collections.Generic;
 
 namespace GK.BRE.BC.BusinessRules
 {
     public class BookBR : IBusinessRule
     {
-        public string Name => throw new NotImplementedException();
+        public string Name => "Book Business Rule";
 
         public Result ExecuteRule(Product product)
         {
-            throw new NotImplementedException();
+            try
+            {
+                if (product.ProductType == ProductType.Book)
+                {
+                    //Create duplicate packing slip for the royalty department
+                    return new Result
+                    {
+                        ResultCode = ResultCode.Success,
+                        Comments = "Created duplicate packing slip for the royalty department successfully"
+                    };
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                return new Result
+                {
+                    ResultCode = ResultCode.Error,
+                    Errors = new List<string> { e.Message }
+                };
+            }
         }
     }
 }
